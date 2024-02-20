ca6a67ead4be7113fdb95527720097f2356870a7
Lorenzo7991 <lorenzo1997chierisini@gmail.com>
2024-02-20 16:14:12 +0100

    added code comments

--- includes/Page.php
+++ includes/Page.php
@@ -1,21 +1,23 @@
+<!-- Import via DIR directive Hotels data for partial path -->
 <?php require_once(__DIR__ . '/../partials/hotel_table.php');
 
+/*  Global variable renamed */
 $data = $_GET;
+/* Check using coalescing operator */
 $parkingFilter = $data['parking'] ?? null;
+/* Flag variable */
 $filteredHotels = '';
 
+/* Loop to filter hotels based on parking key. */
 if ($parkingFilter === '') {
-    $filteredHotels = $hotels;
-} elseif ($parkingFilter === 'all') {
-    $filteredHotels = $hotels;
+    $filteredHotels = $hotels;  //If parkingFilter value is equal to empty string, show all hotels
 } else {
-    $filteredHotels = array_filter($hotels, function ($hotel) use ($parkingFilter) {
-        return ($parkingFilter === 'true' && $hotel['parking']) || ($parkingFilter === 'false' && !$hotel['parking']);
+    $filteredHotels = array_filter($hotels, function ($hotel) use ($parkingFilter) {  // Else, use array_filter function to filter elements of $hotels array, using a callback function.
     });
 }
 
 ?>
-
+<!-- Structure of table, exported on main.php -->
 <div class="table-responsive">
     <table class="table table-dark table-striped">
         <thead>
@@ -28,6 +30,7 @@ if ($parkingFilter === '') {
             </tr>
         </thead>
         <tbody>
+            <!-- Loop to iterate over filtered hotels -->
             <?php foreach ($filteredHotels as $hotel): ?>
                 <tr>
                     <td>
@@ -37,6 +40,7 @@ if ($parkingFilter === '') {
                         <?= $hotel['description']; ?>
                     </td>
                     <td>
+                        <!-- Ternary operator to set correct icon based on the value of "parking" key: -->
                         <?= $hotel['parking'] ? '<i class="fa-solid fa-square-check text-success "></i>' : '<i class="fa-solid fa-square-check text-danger"></i>'; ?>
                     </td>
                     <td>
--- main.php
+++ main.php
@@ -6,34 +6,43 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="assets\favicon.ico" type="image/ico">
     <title>Hotels</title>
-    <!-- Bootstrap -->
+    <!-- Bootstrap CDN -->
     <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.2/css/bootstrap.min.css'
         integrity='sha512-b2QcS5SsA8tZodcDtGRELiGv5SaKSk1vDHDaQRda0htPYWZ6046lr3kJ5bAAQdpV2mmA/4v0wQF9MyU6/pDIAg=='
         crossorigin='anonymous' />
-    <!-- Fontawsome -->
+    <!-- Fontawsome CDN -->
     <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css'
         integrity='sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=='
         crossorigin='anonymous' />
 </head>
 
+<!-- Body dark mode -->
+
 <body class="bg-dark text-white">
+    <!-- Page container -->
     <div class="container py-5">
+        <!-- Header section -->
         <header class="d-flex justify-content-between align-items-center">
+            <!-- Page title -->
             <h1>Hotels</h1>
+            <!-- Form with filter on parkings slot -->
             <form action="" method="GET" class="d-flex align-items-center">
                 <div class="mb-3">
                     <label for="parking" class="form-label">Parking</label>
                     <select name="parking" id="parking" class="form-select">
-                        <option value="">Select a choice</option>
-                        <option value="all">Show all</option>
+                        <!-- <option value="">Select a choice</option> -->
+                        <option value="">Show all</option>
                         <option value="true">Parking</option>
                         <option value="false">NO - Parking</option>
                     </select>
                 </div>
+                <!-- Form btn submit -->
                 <button type="submit" class="btn btn-primary ms-3" style="margin-bottom: -15px">Filtra</button>
             </form>
         </header>
+        <!-- Main section -->
         <main>
+            <!-- Imported main content -->
             <?php include('includes/Page.php') ?>
         </main>
     </div>
